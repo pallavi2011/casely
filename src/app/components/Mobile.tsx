47aebb0a59fe1464b71474428f97794cafd330bb
Pallavi <pallavijorapur20@gmail.com>
2024-10-26 12:13:24 -0700

    finishing landing page

--- src/app/components/Mobile.tsx
+++ src/app/components/Mobile.tsx
@@ -2,16 +2,16 @@ import React, { HTMLAttributes } from 'react'
 import { cn } from "@/lib/utils";
 
 interface MobileProps extends HTMLAttributes<HTMLDivElement>{
-    img:string,
+    imgSrc:string,
     darkmode?:boolean
 }
-const Mobile = ({img, className, darkmode = false, ...props}: MobileProps) => {
+const Mobile = ({imgSrc, className, darkmode = false, ...props}: MobileProps) => {
   return (
     <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}
         {...props}>
             <img src={darkmode ?"/phone-template-dark-edges.png":"/phone-template-white-edges.png"} className='pointer-events-none z-50 select-none' alt="mobile image"/>
             <div className='absolute -z-10 inset-0'>
-                <img className='object-cover' src={img} alt="some image"/>
+                <img className='object-cover' src={imgSrc} alt="some image"/>
             </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { Check,Star } from "lucide-react";
+import { Check, ArrowRight,Star } from "lucide-react";
 import Wrapper from "./components/Wrapper";
 import Mobile from "./components/Mobile";
 import { Icons } from "./components/Icons";
 import { Reviews } from "./components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -66,7 +68,7 @@ export default function Home() {
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
             <div className="relative md:max-w-xl">
               <img src="" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"/>
-              <Mobile className="w-64 " img='/testimonials/1.jpg'/>
+              <Mobile className="w-64 " imgSrc='/testimonials/1.jpg'/>
             </div>
           </div>
         </Wrapper>
@@ -157,6 +159,56 @@ export default function Home() {
         <div className='pt-16'>
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <Wrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">Upload your photo and get {' '}<span className="relative px-2 bg-green-600 text-white">your own case </span>{' '} now</h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img src="/arrow.png" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"/>
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900 lg:rounded-2xl">
+              <img
+                  src='/horse.jpg'
+                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                />
+              </div>
+              <Mobile className='w-60' imgSrc='/horse_phone.jpg' />
+            </div>
+
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="size-5 text-green-600 inline mr-1.5"/>
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="size-5 text-green-600 inline mr-1.5"/>
+              Scratch and finger print resistant
+            </li>
+            <li className="w-fit">
+              <Check className="size-5 text-green-600 inline mr-1.5"/>
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="size-5 text-green-600 inline mr-1.5"/>
+              5 year print warranty
+            </li>
+
+            <div className="flex justify-cneter">
+              <Link href='/configure/upload' className={buttonVariants({
+                size:'lg',
+                className:'mx-auto mt-8'
+              })}>Create your case now <ArrowRight className="size-4 ml-1.5"/></Link>
+            </div>
+          </ul>
+        </Wrapper>
       </section>
     </div>
   );
