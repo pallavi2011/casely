4eb06c339d8abb7d134119ed70d20c6fb5a5b234
Pallavi <pallavijorapur20@gmail.com>
2024-12-04 00:04:58 -0700

    stripe and webhook

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,11 +1,50 @@
 "use client"
 
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import React, {useState, useEffect} from 'react'
+import { getAuthStatus } from './action'
+import { useRouter } from 'next/navigation'
+import { Loader2 } from 'lucide-react'
 
 const page = () => {
+  const [configId, setConfigId] = useState<string | null>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+      const configurationId = localStorage.getItem("configurationId")
+      if(configurationId) setConfigId(configurationId)
+  }, [])
+
+  const {data} = useQuery({
+    queryKey:["auth-callback"],
+    queryFn: async() => await getAuthStatus(),
+    retry: true,
+    retryDelay: 500,
+    
+  })
+
+  if(data?.success){
+    if(configId){
+        localStorage.removeItem("configurationId")
+        router.push(`/configure/preview?id=${configId}`)
+    }
+    else{
+      router.push("/")
+    }
+  }
+
+
   return (
-    <div>page</div>
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col gap-2 items-center'>
+        <Loader2 className='size-8 animate-spin text-zinc-500'/>
+        <h3 className='font-semibold text-xl'>Logging you in...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
   )
 }
 
 export default page
+
+
--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Dialog, DialogHeader, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog'
+import type { Dispatch, SetStateAction } from 'react'
+import Image from 'next/image';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs';
+import { buttonVariants } from '@/components/ui/button';
 
-const LoginModal = () => {
+
+
+const LoginModal = ({isOpen, setIsOpen} :{
+  isOpen: boolean,
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}) => {
   return (
-    <div>LoginModal</div>
+    <Dialog onOpenChange={setIsOpen} open={isOpen}>
+      <DialogContent className="absolute z-[9999999]">
+        <DialogHeader>
+          <div className='relative mx-auto size-24 mb-2'>
+            <Image src="/snake-1.png" alt="snake image" width={500} height={500}className="object-contain"/>
+          </div>
+          <DialogTitle className="text-3xl text-center font-bold tracking-tight text-gray-900 py-2">
+            Log in to continue
+          </DialogTitle>
+          <DialogDescription className="text-base text-center py-2">
+            <span className='font-medium text-zinc-900'>
+              Your configuration was saved!
+            </span>{' '}
+            Please login or create an account to complete your purchase.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='grid grid-cols-2 gap-6 divide-x divide-gray-200'>
+          <LoginLink className={buttonVariants({ variant: 'outline' })}>
+            Login
+          </LoginLink>
+          <RegisterLink className={buttonVariants({variant:'default'})}>
+            Sign Up
+          </RegisterLink>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 
